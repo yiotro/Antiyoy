655635449372d8002622bb1fd6c5c75605c00c39
David Poetzsch-Heffter <davidpoetzsch@web.de>
2018-02-11 14:11:01 +0100

    copied project files generated for this app via libgdx project generator (according to README)

---
+++ desktop/src/yio/tro/antiyoy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package yio.tro.antiyoy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import yio.tro.antiyoy.YioGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new YioGdxGame(), config);
+	}
+}
